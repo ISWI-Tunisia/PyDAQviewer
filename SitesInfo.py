5b91ea12c47404070a18a406abb9c8fc36d474d8
astrax <ammarahmed.ph@gmail.com>
2018-03-30 22:33:19 +0100

    add some VLF Transmitters

--- SitesInfo.py
+++ SitesInfo.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 '''
 Author: Ahmed Ammar, ahmed.ammar@fst.utm.tn
-Purpose: Savde VLF Transmitters and Receivers Info
+Purpose: Save VLF Receivers (Rx) and Transmitters (Tx) Info
 Date Created: Mon Mar 12 22:23:34 2018
    
 '''
@@ -24,6 +24,23 @@ Tx_ID = {
             "NLK-NS":"NLK_000",
             "NLK-EW":"NLK_001",
             "NAU-NS":"NAU_000",
-            "NAU-EW":"NAU_001"
-            
+            "NAU-EW":"NAU_001",
+            "NPM-NS":"NPM_000",
+            "NPM-EW":"NPM_001",
+            "ICV-NS":"ICV_000",
+            "ICV-EW":"ICV_001",
+            "NSC-NS":"NSC_000",
+            "NSC-EW":"NSC_001",
+            "GQD-NS":"GQD_000",
+            "GQD-EW":"GQD_001",
+            "GBZ-NS":"GBZ_000",
+            "GBZ-EW":"GBZ_001",
+            "DHO-NS":"DHO_000",
+            "DHO-EW":"DHO_001",
+            "HWU-NS":"HWU_000",
+            "HWU-EW":"HWU_001",
+            "JXN-NS":"JXN_000",
+            "JXN-EW":"JXN_001",
+            "ISR-NS":"ISR_000",
+            "ISR-EW":"ISR_001"
             }
